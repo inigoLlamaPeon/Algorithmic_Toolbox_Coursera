e1ef81fae9370cc9936f3b75719d48bbb86867fb
Iñigo de la Llama <70801334+inigoLlamaPeon@users.noreply.github.com>
2021-10-21 11:49:51 +0200

    Update fiboncacciNumberAgain.c

--- Week_2/fiboncacciNumberAgain.c
+++ Week_2/fiboncacciNumberAgain.c
@@ -18,13 +18,13 @@ int main()
 uint64_t fibonacciAgain(uint64_t N, uint16_t m)
 {
 	uint16_t L = pisano(m);
-    uint64_t remainder;
-    do
-    {
-        remainder = N % L;
-        N = remainder;
-    } while (remainder >= L);
-    return ((uint64_t)fibonacciMod(N, m));
+	uint64_t remainder;
+	do
+	{
+		remainder = N % L;
+		N = remainder;
+	} while (remainder >= L);
+	return ((uint64_t)fibonacciMod(N, m));
 }
 
 uint32_t fibonacciMod(uint32_t N, uint16_t m)
@@ -42,14 +42,14 @@ uint32_t fibonacciMod(uint32_t N, uint16_t m)
 uint16_t pisano(uint16_t m)
 {
 	int n1 = -1, n2 = -1, i = 2;
-    if (m == 1)
-        return 0;
-    while (1)
-    {
-        n2 = fibonacciMod((uint32_t)i, m);
-        if (n2 == 1 && n1 == 0)
-            return i - 1;
-        n1 = n2;
-        i++;
-    }
+    	if (m == 1)
+        	return 0;
+    	while (1)
+    	{
+        	n2 = fibonacciMod((uint32_t)i, m);
+        	if (n2 == 1 && n1 == 0)
+            		return i - 1;
+        	n1 = n2;
+        	i++;
+    	}
 }
