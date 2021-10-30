c875f52bb435256b6184e94eb646ad9c8becb39e
Iñigo de la Llama <70801334+inigoLlamaPeon@users.noreply.github.com>
2021-10-30 12:15:07 +0200

    Add files via upload

---
+++ Week_3/3_carFueling.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdint.h>
+
+#define MAX_STATION 300
+
+int numRefill(int32_t distance, int32_t milesTank, int32_t *station, int32_t nStations);
+
+int main()
+{
+	int32_t distance, milesTank, nStations, station[MAX_STATION];
+	scanf("%ld", &distance);
+	scanf("%ld", &milesTank);
+	scanf("%ld", &nStations);
+	for(int16_t i = 0; i < nStations; i++)
+		scanf("%ld", &station[i]);
+	printf("%d", numRefill(distance, milesTank, station, nStations));
+	return 0;
+}
+
+int numRefill(int32_t distance, int32_t milesTank, int32_t *station, int32_t nStations)
+{
+	int cnt = 0;
+	int32_t i = 0, position = 0, stop = 0;
+	station[nStations] = distance;
+	while (i <= nStations)
+    {
+        if (position + milesTank >= distance)
+            return cnt;
+        if (position + milesTank < station[i])
+        {
+            stop = i - 1;
+            if (position == station[stop])
+                return -1;
+            position = station[i - 1];
+			cnt++;
+            i--;
+        }
+        i++;
+    }
+	return -1;
+}
---
+++ Week_3/4_maximumAdvertisementRevenue.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdint.h>
+
+int64_t maxAddRevenue(int64_t *src1, int64_t *src2, int n);
+void *sortArray(int64_t *src, int n);
+
+int main()
+{
+	int n;
+	int64_t a[1000], b[1000];
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++)
+		scanf("%lld", &a[i]);
+	for (int j = 0; j < n; j++)
+		scanf("%lld", &b[j]);
+	printf("%lld", maxAddRevenue(a, b, n));
+	return 0;
+}
+
+int64_t maxAddRevenue(int64_t *src1, int64_t *src2, int n)
+{
+	int64_t result = 0;
+	sortArray(src1, n);
+	sortArray(src2, n);
+	for(int i = 0; i < n; i++)
+		result += src1[i] * src2[i];
+	return result;
+}
+void *sortArray(int64_t *src, int n)
+{
+	int64_t aux;
+	for(int i = 0; i < n; i++)
+	{
+		for(int j = 0; j < n; j++)
+		{
+			if (src[i] > src[j]) 
+            {
+                aux = src[i];
+                src[i] = src[j];
+                src[j] = aux;
+            }
+		}
+	}
+}
